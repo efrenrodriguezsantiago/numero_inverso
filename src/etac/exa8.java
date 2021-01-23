4db312fe8d47e3851bc0fdf90a4e8b4aa799ee22
Efren Rodriguez <1717110246@utectulancingo.edu.mx>
2021-01-23 00:34:04 -0600

    producto de artefacto lavadora planchas etc.

---
+++ src/etac/exa8.java
@@ -0,0 +1,15 @@
+package etac;
+import java.util.*; 
+public class exa8 {
+	//.- Un cliente realiza la siguiente compra de artefactos, plancha, licuadora, lavadora y cocina, 
+	//si la compra supera los 1200, recibe un descuento de 17 %, mostrar el precio de cada artefacto, a
+	//sí como el monto, el descuento y el pago total si le corresponde un descuento. 
+
+	 public static void main(String []args){ 
+	 
+	 
+	 
+	 }
+	 
+	 
+}
