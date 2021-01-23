6621e860a920329251afe7eeb9e6a7849f245b1a
Efren Rodriguez <1717110246@utectulancingo.edu.mx>
2021-01-23 00:17:07 -0600

    mostrar productos

---
+++ src/etac/exa1.java
@@ -0,0 +1,17 @@
+package etac;
+
+public class exa1 {
+	public static class Ejercicio1 {
+		 public static void main(String[]args){ 
+			 
+		//Empleando arreglos, mostrar n productos con sus respectivos precios
+		String producto []={"huevo", "leche","jabon","pollo"};
+		 double precios[]={36,17.50,12.07,70};
+		 System.out.println("Lista de Productos y sus precios");
+		 for (int u=0;u<producto.length;u++){
+		 System.out.println(producto[u]+ " "+ precios[u]); 
+		 
+		    } 
+		}
+	}
+}
